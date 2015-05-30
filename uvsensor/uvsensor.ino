fc62eb495ffbed9c712d85e5f255f92de20788ac
Ryan Yacyshyn <ryan.yacyshyn@gmail.com>
2015-05-30 23:28:50 +0800

    sending value to server..

--- uvsensor/uvsensor.ino
+++ uvsensor/uvsensor.ino
@@ -13,11 +13,11 @@
 #define MAC_ADDR_LEN    6   // Length of MAC address in bytes
 
 // Constants
-char ap_ssid[] = "WIFI_JU_1205";                      // SSID of network
-char ap_password[] = "sg207229";                      // Password of network
+char ap_ssid[] = "rye";                               // SSID of network
+char ap_password[] = "Vancouver";                     // Password of network
 unsigned int ap_security = WLAN_SEC_WPA2;             // Security of network
 unsigned int timeout = 30000;                         // Milliseconds
-char server[] = "arduino-temp-monitor.herokuapp.com"; // Remote host site
+char server[] = "uvi-monitor.herokuapp.com";          // Remote host site
 uint16_t port = 80;
 
 String ip_address = "";
@@ -59,33 +59,28 @@ void setup() {
   Serial.print("MAC Address: ");
   Serial.println(mac_address);
 
-  // sendStats();
+  sendUVI();
   
 }
 
 void loop() {
 
   // If there are incoming bytes, print them
-  // if (client.available()) {
-  //   char c = client.read();
-  //   Serial.print(c);
-  // }
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
   
-
-
   // If the server has disconnected, stop the client and wifi
-  // if (!client.connected()) {
-  //   Serial.println();
-  //   Serial.println("closing client..");
-  //   if (!client.close()) {
-  //     Serial.println("Error: Could not close socket");
-  //   }
-  //   delay(60000); // wait a minute..
-  //   sendStats();
-  // }
-
-
-
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("closing client..");
+    if (!client.close()) {
+      Serial.println("Error: Could not close socket");
+    }
+    delay(1000);
+    sendUVI();
+  }
   
   // If the server has disconnected, stop the client and wifi
   // if ( !client.connected() ) {
@@ -109,43 +104,42 @@ void loop() {
   // }
 }
 
-// void sendStats () {
-//   float temp = getTemperature();
-//   float humidity = getHumidity();
-//   float light = getLight();
+void sendUVI () {
 
-//   sendData(temp, humidity, light);
-// }
+  float uvi = 4.58;
 
-// void sendData (float temp, float humidity, float light) {
-//   // Make a TCP connection to remote host
-//   Serial.print("Performing HTTP POST to: ");
-//   Serial.print(server);
-//   Serial.print(" - port: ");
-//   Serial.println(port);
+  postData(uvi);
+}
 
-//   if (!client.connect(server, port)) {
-//     Serial.println("Error: Could not make a TCP connection");
-//   }
+void postData (float uvi) {
+  // Make a TCP connection to remote host
+  Serial.print("Performing HTTP POST to: ");
+  Serial.print(server);
+  Serial.print(" - port: ");
+  Serial.println(port);
 
-//   String data = "t=" + String(temp) + "&h=" + String(humidity) + "&l=" + String(light) + "&ip=" + ip_address + "&mac=" + mac_address;
+  if (!client.connect(server, port)) {
+    Serial.println("Error: Could not make a TCP connection");
+  }
 
-//   // Make a HTTP POST request..
-//   client.println("POST /submit_data HTTP/1.1");
-//   client.print("Host: ");
-//   client.println(server);
-//   client.println("Connection: close");
-//   client.println("Content-Type: application/x-www-form-urlencoded");
-//   client.print("Content-Length: ");
-//   client.println(data.length());
-//   client.println();
-//   client.println(data);
+  String data = "uvi=" + String(uvi) + "&ip=" + ip_address + "&mac=" + mac_address;
+
+  // Make a HTTP POST request..
+  client.println("POST /submit_data HTTP/1.1");
+  client.print("Host: ");
+  client.println(server);
+  client.println("Connection: close");
+  client.println("Content-Type: application/x-www-form-urlencoded");
+  client.print("Content-Length: ");
+  client.println(data.length());
+  client.println();
+  client.println(data);
   
-//   client.println();
+  client.println();
 
-//   Serial.println();
+  Serial.println();
 
-// }
+}
 
 String getIPAddress () {
   
@@ -194,30 +188,3 @@ String getMacAddress () {
   } 
   return mac;
 }
-
-// float getTemperature() {
-//   int value_temp = analogRead(T);
-//   delay(10);
-//   value_temp = analogRead(T);
-//   delay(10);
-//   float millivolts_temp = (value_temp / 1023.0) * 5000;
-//   return millivolts_temp / 10;
-// }
-
-// float getHumidity() {
-//   int value_hum = analogRead(H);
-//   delay(10);
-//   value_hum = analogRead(H);
-//   delay(10);
-//   float millivolts_hum = (value_hum / 1023.0) * 5000;
-//   return millivolts_hum / 50;
-// }
-
-// float getLight() {
-//   int value_lig = analogRead(L);
-//   delay(10);
-//   value_lig = analogRead(L);
-//   delay(10);
-//   float millivolts_light = (value_lig / 1023.0) * 5000;
-//   return millivolts_light / 10;
-// }
